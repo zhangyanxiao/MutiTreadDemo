fb285c8b184debc4bd2a1e7e245fe10efee7c2aa
zhangyanxiao <385584895@qq.com>
2017-04-01 11:18:02 +0800

    死锁的认识

--- MutiTreadDemo/TaskViewController.m
+++ MutiTreadDemo/TaskViewController.m
@@ -66,7 +66,7 @@
         make.right.equalTo(self.view).offset(-20);
         make.bottom.equalTo(self.controlBtn.mas_top).offset(-20);
     }];
-    self.textView.text = @"主队列，同步添加多个任务\n会crash！！！！！\n原因如下：\n1、主队列在执行dispatch_sync函数,函数会把一个block加入到指定的队列，此函数要求执行完block才返回.\n2、dispatch_sync函数把block加入的指定队列，是主队列时。就会出现下面的状况：\n \n原主队列有A,B,C三个任务，在A任务完成后，执行B任务时，突然需要在B中嵌套一个D任务，按照先进先出的原则，现在主队列有未完成的任务B和待执行任务C,D，但是有一个要求，D要求它先执行完再执行其他的,此时我们可以发现队列中的任务产生了冲突，D等待主队列完成BC任务后执行，但是B完成任务的前提是D完成任务。造成死锁";
+    self.textView.text = @"主队列，同步添加多个任务\n会crash！！！！！\n原因如下：\n1、主队列在执行dispatch_sync函数,函数会把一个block加入到指定的队列，此函数要求执行完block才返回.\n2、dispatch_sync函数把block加入的指定队列，是主队列时。就会出现下面的状况：\n \n原主队列有A,B三个任务，在A任务完成后，执行B任务时，突然需要在B中嵌套一个D任务，按照先进先出的原则，现在主队列有正在进行中的任务B和待执行任务D，但是有一个要求，D要求它先执行完再执行其他的,此时我们可以发现队列中的任务产生了冲突，D要求它先执行完再执行其他的，但是B完成任务的前提是D完成任务。造成死锁";
     
 }
 #pragma mark ---响应方法
@@ -160,7 +160,9 @@
 
     
     dispatch_queue_t mainqueue = dispatch_get_main_queue();
+    
     [self syncAddWithQueue:mainqueue];
+    [self task4];
 
 }
 -(void)globalQueueSync{
@@ -174,33 +176,36 @@
     self.textView.text = @"串行队列，同步添加多个任务\n1、同步——不开新线程 \n2、当前在主线程，所以4个任务运行在主线程中；\n3、虽然是串行队列，但是只有一个线程，所以任务仍然根据添加顺序依次执行；\n4、同步函数的特点执行完一个，再执行下一个；\n\n串行队列为什么在此时不会卡死呢？解释如下：\n主队列有A,B,C三个任务，在A任务完成后，执行B任务时，突然创建了一个串行队列，串行队列里添加了一个E,F,G三个任务，按照先进先出的原则，主队列有未完成的任务B和待执行任务C,D;串行队列有E,F,G三个任务，但是任务B和串行队列并不在一个队列中，线程此时执行B开始后，继续执行串行队列中的EFG三任务后，回到任务B完成，再继续以后的任务";
     dispatch_queue_t serialqueue = dispatch_queue_create("serialqueue", DISPATCH_QUEUE_SERIAL);
     [self syncAddWithQueue:serialqueue];
+    [self task4];
 }
 -(void)concurrentQueueSync{
     self.textView.text = @"并发队列，同步添加多个任务\n1、同步——不开新线程 \n2、当前在主线程，所以4个任务运行在主线程中；\n3、虽然是并发队列，但是只有一个线程，所以任务仍然根据添加顺序依次执行；\n4、同步函数的特点执行完一个，再执行下一个；";
     dispatch_queue_t concurrentqueue = dispatch_queue_create("concurrentqueue", DISPATCH_QUEUE_CONCURRENT);
     [self syncAddWithQueue:concurrentqueue];
+    [self task4];
     
 }
 
 -(void)syncAddWithQueue:(dispatch_queue_t)queue{
     dispatch_sync(queue, ^{
+        
         [self task1];
-        [self performSelectorOnMainThread:@selector(refreshView) withObject:nil waitUntilDone:YES];
+//        [self performSelectorOnMainThread:@selector(refreshView) withObject:nil waitUntilDone:YES];
     });
     dispatch_sync(queue, ^{
         [self task2];
-        [self performSelectorOnMainThread:@selector(refreshView) withObject:nil waitUntilDone:YES];
+//        [self performSelectorOnMainThread:@selector(refreshView) withObject:nil waitUntilDone:YES];
     });
     
     dispatch_sync(queue, ^{
         [self task3];
-        [self performSelectorOnMainThread:@selector(refreshView) withObject:nil waitUntilDone:YES];
+//        [self performSelectorOnMainThread:@selector(refreshView) withObject:nil waitUntilDone:YES];
     });
     
-    dispatch_sync(queue, ^{
-        [self task4];
-        [self performSelectorOnMainThread:@selector(refreshView) withObject:nil waitUntilDone:YES];
-    });
+//    dispatch_sync(queue, ^{
+//        [self task4];
+////        [self performSelectorOnMainThread:@selector(refreshView) withObject:nil waitUntilDone:YES];
+//    });
 }
 
 -(void)mainQueueAsync{
@@ -220,6 +225,7 @@
     // 1-->2(完成)-->3-->4 子线程
     dispatch_queue_t serialqueue = dispatch_queue_create("serialqueue", DISPATCH_QUEUE_SERIAL);
     [self asyncAddWithQueue:serialqueue];
+    [self task4];
 }
 -(void)concurrentQueueAsync{
     self.textView.text = @"并发队列，异步添加多个任务\n\n1、异步，开线程，不确定开几条线程\n2、当前在主线程，异步函数dispatch_async开线程，主线程中只执行task5，异步添加的任务都会在各子线程执行\n3、并发队列，所以任务会同时进行，并且线程各不相同（也有相同的）\n4、并发队列：也遵循FIFO原则，但时间太快可忽略不计，取出所有任务同时执行。所以任务中task的开始顺序，不确定，结束顺序更不确定，因为谁快谁结束早";
@@ -239,9 +245,9 @@
         [self task3];
     });
     
-    dispatch_async(queue, ^{
-        [self task4];
-    });
+//    dispatch_async(queue, ^{
+//        [self task4];
+//    });
     dispatch_async(queue, ^{
         [self task1];
     });
@@ -253,9 +259,9 @@
         [self task3];
     });
     
-    dispatch_async(queue, ^{
-        [self task4];
-    });
+//    dispatch_async(queue, ^{
+//        [self task4];
+//    });
     
 }
 
